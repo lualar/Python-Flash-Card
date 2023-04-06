8485a81528149bd45968b0914dcde2c70a76b6e0
AlfonsoAlvarez <lualar@gmail.com>
2023-04-06 09:56:52 -0500

    final version

--- FrenchWords.py
+++ FrenchWords.py
@@ -2,11 +2,19 @@ import pandas as pd
 
 class cRandomWord():
     def __init__(self):
-        self.dfFrenchWords = pd.read_csv("./data/french_words.csv").to_dict(orient="records")
-        print (f"French Words: {self.dfFrenchWords}")
+        #open file 
+        try:
+            #If user already played this app before and save its status
+            self.dfFrenchWords = pd.read_csv("./data/words_to_learn.csv").to_dict(orient="records")
+        except FileNotFoundError:
+            self.dfFrenchWords = pd.read_csv("./data/french_words.csv").to_dict(orient="records")
+        #print (f"French Words: {self.dfFrenchWords} - Count {len(self.dfFrenchWords)}")
        
     def dGetDict(self):
         return self.dfFrenchWords
+
+    def fSaveWordsToLearn(self, dictWordsToLearn):
+        pd.DataFrame.to_csv(pd.DataFrame.from_dict(dictWordsToLearn), "./data/words_to_learn.csv", index=False)
 
 
 
--- Python_Flash_Card.py
+++ Python_Flash_Card.py
@@ -1,3 +1,4 @@
+from os import remove
 from tkinter import *
 from FrenchWords import cRandomWord
 import random
@@ -21,8 +22,8 @@ def NextCard():
     #get a random card from Dict
     Current_Card = random.choice(fDict)
     
-    #get French and English words from random card
-    print(f"Words: French: {Current_Card['French']} - English: {Current_Card ['English']}")
+    #Print French and English words from random card
+    #print(f"Words: French: {Current_Card['French']} - English: {Current_Card ['English']}")
 
     #assing french word to French field in window
     myCanvas.itemconfig(myCanvasImage, image=imFrontMyImage)    #Change card image to front
@@ -31,16 +32,27 @@ def NextCard():
     #Wait for some time a flip the card
     tFlipTimer = wMyWindow.after(TIME_TO_FLIP, func=FlipCard)
 
+#User already know the word. Remove this card from the dictionary
+def NextCardOK():
+    RemoveCard() #Remove the flash card from dict
+    NextCard()  #Get next flash card
+ 
+#Function flip the card and show the English translation of the word
 def FlipCard():
     global Current_Card  #Storage the current flash card
     myCanvas.itemconfig(myCanvasImage, image=imBackImage)    #Change card image to back
     myCanvas.itemconfig(cardFrenchTitle, text="English")    #change title from French to English
     myCanvas.itemconfig(cardFrenchWord, text=Current_Card ['English'], fill="white")
 
+#remove card from dict and save the new list on CSV file
+def RemoveCard():
+    fDict.remove(Current_Card)
+    cWord.fSaveWordsToLearn(fDict)
 
 #configure window
 wMyWindow = Tk()
-wMyWindow.title("Flash Cards")
+sTitle = "Flash Cards: " + str(iWords)  + " words to play!"
+wMyWindow.title(sTitle)
 wMyWindow.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 #Wait for some time a flip the card
@@ -71,7 +83,7 @@ check_image = PhotoImage(file="./images/right.png")
 myUnknowButton = Button(image=cross_image, highlightthickness=0, command=NextCard)
 myUnknowButton.grid(row=1, column=0)
 
-myCheckButton = Button(image=check_image, highlightthickness=0, command=NextCard)
+myCheckButton = Button(image=check_image, highlightthickness=0, command=NextCardOK)
 myCheckButton.grid(row=1, column=1)
 
 #Get 1st random Card
